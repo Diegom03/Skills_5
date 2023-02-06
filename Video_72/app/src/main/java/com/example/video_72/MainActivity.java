fe0500bed68b0f7563cb7b58d64bf2dd48e0991b
Diegom03 <diegomartinm03@gmail.com>
2023-02-06 21:02:11 +0100

    Commit 1.2

--- Video_72/app/src/main/java/com/example/video_72/MainActivity.java
+++ Video_72/app/src/main/java/com/example/video_72/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.video_72;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> listDatos;
+    RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recycler = (RecyclerView) findViewById(R.id.recyclerId);
+        recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        listDatos = new ArrayList<String>();
+
+        for(int i = 0; i < 50; i++) {
+            listDatos.add("Dato" + i + " ");
+        }
+
+        AdapterDatos adapter = new AdapterDatos(listDatos);
+
+        recycler.setAdapter(adapter);
     }
 }
