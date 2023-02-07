06c237adeae9daa28d07065bd6fabe1922d38634
Diegom03 <diegomartinm03@gmail.com>
2023-02-07 19:59:58 +0100

    Commit 1.5

---
+++ Video_72/app/src/main/java/com/example/video_72/AdaptadorPersonajes.java
@@ -0,0 +1,51 @@
+package com.example.video_72;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdaptadorPersonajes extends RecyclerView.Adapter<AdaptadorPersonajes.ViewHolderPersonajes> {
+
+    ArrayList<PersonajeVo> listaPersonajes;
+
+    public AdaptadorPersonajes(ArrayList<PersonajeVo> listaPersonajes) {
+        this.listaPersonajes = listaPersonajes;
+    }
+
+    @Override
+    public AdaptadorPersonajes.ViewHolderPersonajes onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_personajes, null, false);
+        return new ViewHolderPersonajes(view);
+    }
+
+    @Override
+    public void onBindViewHolder(AdaptadorPersonajes.ViewHolderPersonajes holder, int position) {
+        holder.etiNombre.setText(listaPersonajes.get(position).getNombre());
+        holder.etiInformacion.setText(listaPersonajes.get(position).getInfo());
+        holder.foto.setImageResource(listaPersonajes.get(position).getFoto());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaPersonajes.size();
+    }
+
+    public class ViewHolderPersonajes extends RecyclerView.ViewHolder {
+        TextView etiNombre, etiInformacion;
+        ImageView foto;
+
+        public ViewHolderPersonajes(@NonNull View itemView) {
+            super(itemView);
+            etiNombre = (TextView) itemView.findViewById(R.id.idNombre);
+            etiInformacion = (TextView) itemView.findViewById(R.id.idInfo);
+            foto = (ImageView) itemView.findViewById(R.id.idImagen);
+        }
+    }
+}
