9cd7ee919317f96a187e73f1448aca612eaff2f3
Diegom03 <diegomartinm03@gmail.com>
2023-02-09 19:45:02 +0100

    Commit 1.9

--- Retrofit2/app/src/main/java/com/example/retrofit2/movie_list/MovieListModel.java
+++ Retrofit2/app/src/main/java/com/example/retrofit2/movie_list/MovieListModel.java
@@ -1,4 +1,50 @@
 package com.example.retrofit2.movie_list;
 
-public class MovieListModel {
+import android.util.Log;
+
+import com.example.retrofit2.model.Movie;
+import com.example.retrofit2.model.MovieListResponse;
+import com.example.retrofit2.network.ApiClient;
+import com.example.retrofit2.network.ApiInterface;
+
+import java.util.List;
+
+import javax.security.auth.callback.Callback;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class MovieListModel implements MovieListContract.Model {
+    private final String TAG = "MovieListModel";
+
+
+    /**
+     * This function will fetch movies data
+     * @param onFinishedListener
+     * @param pageNo : Which page to load.
+     */
+    @Override
+    public void getMovieList(final MovieListContract.Model.OnFinishedListener onFinishedListener, int pageNo) {
+
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        Call<MovieListResponse> call = apiService.getPopularMovies(API_KEY, pageNo);
+        call.enqueue(new Callback<MovieListResponse>() {
+            @Override
+            public void onResponse(Call<MovieListResponse> call, Response<MovieListResponse> response) {
+                List<Movie> movies = response.body().getResults();
+                Log.d(TAG, "Number of movies received: " + movies.size());
+                onFinishedListener.onFinished(movies);
+            }
+
+            @Override
+            public void onFailure(Call<MovieListResponse> call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+                onFinishedListener.onFailure(t);
+            }
+        });
+    }
+
 }
--- Retrofit2/app/src/main/java/com/example/retrofit2/movie_list/MovieListPresenter.java
+++ Retrofit2/app/src/main/java/com/example/retrofit2/movie_list/MovieListPresenter.java
@@ -1,4 +1,56 @@
 package com.example.retrofit2.movie_list;
 
-public class MovieListPresenter {
+import com.example.retrofit2.model.Movie;
+
+import java.util.List;
+
+public class MovieListPresenter implements MovieListContract.Presenter, MovieListContract.Model.OnFinishedListener {
+    private MovieListContract.View movieListView;
+
+    private MovieListContract.Model movieListModel;
+
+    public MovieListPresenter(MovieListContract.View movieListView) {
+        this.movieListView = movieListView;
+        movieListModel = new MovieListModel();
+    }
+
+    @Override
+    public void onDestroy() {
+        this.movieListView = null;
+    }
+
+    @Override
+    public void getMoreData(int pageNo) {
+
+        if (movieListView != null) {
+            movieListView.showProgress();
+        }
+        movieListModel.getMovieList(this, pageNo);
+    }
+
+    @Override
+    public void requestDataFromServer() {
+
+        if (movieListView != null) {
+            movieListView.showProgress();
+        }
+        movieListModel.getMovieList(this, 1);
+    }
+
+    @Override
+    public void onFinished(List<Movie> movieArrayList) {
+        movieListView.setDataToRecyclerView(movieArrayList);
+        if (movieListView != null) {
+            movieListView.hideProgress();
+        }
+    }
+
+    @Override
+    public void onFailure(Throwable t) {
+
+        movieListView.onResponseFailure(t);
+        if (movieListView != null) {
+            movieListView.hideProgress();
+        }
+    }
 }
